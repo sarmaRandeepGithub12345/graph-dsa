6eb0c78bf30e8596990d3958d5c303dbb169cf49
Randeep Sarma <92437704+randeepsarma@users.noreply.github.com>
2022-12-03 16:09:50 +0530

    Create flood_fill_algorithm.cpp

---
+++ flood_fill_algorithm.cpp
@@ -0,0 +1,23 @@
+vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        // Code here 
+        int oldColor=image[sr][sc];
+    
+     vector<int>X={0,0,1,-1};
+     vector<int>Y={1,-1,0,0};
+    queue<pair<int,int>>q;
+    q.push({sr,sc});
+    image[sr][sc]=newColor;
+    while(!q.empty()){
+    int i=q.front().first;
+    int j=q.front().second;
+        q.pop();
+    for(int k=0;k<4;k++){
+    int x=i+X[k],y=j+Y[k];
+    if(x>=0 && x<image.size() && y>=0 && y<image[0].size() && image[x][y]==oldColor && oldColor!=newColor){
+    image[x][y]=newColor;
+    q.push({x,y});
+    }
+ }    
+    }
+    return image;    
+    }
