4189afb97324294d1fd1de4ba325a61dc315ed7b
Randeep Sarma <92437704+randeepsarma@users.noreply.github.com>
2022-12-06 14:26:30 +0530

    Create Topological_sort.cpp
    This is possible only in directed acyclic graph  or DAG . This node which has an indegree of 0 is pushed in first(or say the node which does not have any node coming into it )

---
+++ Topological_sort.cpp
@@ -0,0 +1,26 @@
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int>indegree(V,0);
+	  for(int i=0;i<V;i++){
+	      for(int j=0;j<adj[i].size();j++){
+	          int t=adj[i][j];
+	          indegree[t]++;
+	    }
+	  }
+	stack<int>sq;
+	for(int i=0;i<V;i++){
+	    if(!indegree[i])sq.push(i);
+	}
+	  vector<int>ans;
+	  while(!sq.empty()){
+	      int tp=sq.top();
+	      ans.push_back(tp);
+	      sq.pop();
+	      for(int i=0;i<adj[tp].size();i++){
+	          int t =adj[tp][i];
+	          indegree[t]--;
+	          if(!indegree[t])sq.push(t);
+	      }  
+	  }
+	  return ans;
+	}
