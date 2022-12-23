4d36893ad01a1b98e4e0a5040f290e5e43e1086e
Randeep Sarma <92437704+randeepsarma@users.noreply.github.com>
2022-12-24 01:01:37 +0530

    Create kruskal's_algorithm.cpp

---
+++ kruskal's_algorithm.cpp
@@ -0,0 +1,49 @@
+ int find(vector<int>&parent,int node){
+        if(parent[node]==node)return node;
+        else parent[node]=find(parent,parent[node]);
+    }
+    void unionfunct(int u,int v,vector<int>&parent,vector<int>&dist){
+        int lu= find(parent,u);
+        int lv= find(parent,v);
+        if(dist[lv]<dist[lu]){
+            parent[lv]=lu;
+            dist[lu]+=dist[lv];
+            
+        }else{
+            parent[lu]=lv;
+            dist[lv]+=dist[lu];
+        }
+    }
+    
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        vector<pair<int,pair<int,int>>>temp;
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                int u=it[0];
+                int d=it[1];
+         //  cout<<i<<" "<<u<<" "<<d<<endl;
+                temp.push_back({d,{i,u}});
+            }
+        }
+    vector<int>parent(V);
+    vector<int>dist(V);
+    for(int i=0;i<V;i++){
+        parent[i]=i;
+        dist[i]=1;
+    }
+    int sum=0;
+    sort(temp.begin(),temp.end());    
+   // cout<<temp[0].first<<" "<<temp[0].second.first<<" "<<temp[0].second.second<<endl;
+    for(auto it:temp){
+    int d=it.first,u=it.second.first,v=it.second.second;    
+    
+    if(find(parent,u)!=find(parent,v)){
+   // cout<<u<<" "<<v<<" "<<d<<endl;
+unionfunct(u,v,parent,dist);
+    sum+=d;
+    }    
+    }    
+        
+    return sum;    
+    }
